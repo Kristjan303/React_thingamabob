302d95ff43034c585921ffe1fa863cacb710a15e
Kristjan303 <kristjan.puusepp@voco.ee>
2023-04-18 12:23:59 +0300

    Change Title button wip

--- expens/src/components/expenses/ExpenseItem.js
+++ expens/src/components/expenses/ExpenseItem.js
@@ -1,16 +1,21 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+    const clickHandler = () => {
+        console.log('Clicked')
+    }
 
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
         <ExpenseDate expenseDate={props.expenseData.expenseDate}></ExpenseDate>
       <div className='expense-item__description'>
         <h2>{props.expenseData.expenseTitle}</h2>
         <div className='expense-item__price'>{props.expenseData.expenseAmount}</div>
       </div>
-    </div>
+        <button onClick={clickHandler}>Change Title</button>
+    </Card>
   )
 }
 
